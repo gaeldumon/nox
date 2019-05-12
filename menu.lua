9675c21adc81a0c7875ee028ce809a4cbabc64cd
hyperdestru <slotuser@gmail.com>
2019-05-12 02:16:43 +0200

    Minor bug modif with crafting and refac

--- menu.lua
+++ menu.lua
@@ -3,15 +3,16 @@ local menu = {}
 local rain = {}
 counter = 0
 
-function CreateDrops()
+function MakeRain()
 	local drop = {}
 	drop.x = love.math.random(GAME_WIDTH / 2 - 200, GAME_WIDTH / 2 + 200)
 	drop.y = 0
 	drop.vy = 0
-	drop.speed = 2
+	drop.gravity = 6
 	drop.width = 2
 	drop.height = 10
 	drop.color = {0, 255, 0}
+	drop.delete = false
 
 	table.insert(rain, drop)
 end
@@ -24,14 +25,19 @@ function menu.Update(dt)
 	counter = counter + 1 * (60*dt)
 
 	if counter >= 5 then
-		CreateDrops()
+		MakeRain()
 		counter = 0
 	end
 
 	local i
 	for i, drop in ipairs(rain) do
-		drop.vy = drop.speed * (60*dt)
+		drop.vy = drop.gravity * (60*dt)
 		drop.y = drop.y + drop.vy
+
+		if drop.y > GAME_HEIGHT then
+			drop.delete = true
+			table.remove(rain, i)
+		end
 	end
 end
 
