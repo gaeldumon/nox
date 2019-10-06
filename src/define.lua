eba554909a94be405d6afdf68fca5ef613ce6de6
= <slotuser@gmail.com>
2019-10-06 19:50:59 +0200

    Added some colors that were absent

--- src/define.lua
+++ src/define.lua
@@ -4,6 +4,18 @@ this.current_screen = 'menu'
 this.SCREEN_WIDTH = love.graphics.getWidth()
 this.SCREEN_HEIGHT = love.graphics.getHeight()
 
+this.color = {}
+this.color.white = {1,1,1}
+this.color.black = {0,0,0}
+this.color.grey = {0.5,0.5,0.5}
+this.color.red = {1,0,0}
+this.color.dark_red = {188/255, 24/255, 9/255}
+this.color.green = {0,1,0}
+this.color.blue = {0,0,1}
+this.color.sun_yellow = {244/255, 240/255, 2/255}
+this.color.brown = {102/255, 51/255, 0}
+this.color.startrek_blue = {101/255,117/255,166/255}
+
 function this.update_sprites(p_table, dt)
 	local n
 	for n, sprite in ipairs(p_table) do
