a6c4c0ad5b876229fbbaa452aa74c86fdbe619f4
= <slotuser@gmail.com>
2019-10-06 18:47:29 +0200

    Added generic sprites functions that were missing (old project)

--- src/define.lua
+++ src/define.lua
@@ -1,8 +1,46 @@
-local def = {}
+local this = {}
 
-def.current_screen = 'menu'
-def.SCREEN_WIDTH = love.graphics.getWidth()
-def.SCREEN_HEIGHT = love.graphics.getHeight()
+this.current_screen = 'menu'
+this.SCREEN_WIDTH = love.graphics.getWidth()
+this.SCREEN_HEIGHT = love.graphics.getHeight()
 
-return def
+function this.update_sprites(p_table, dt)
+	local n
+	for n, sprite in ipairs(p_table) do
+		if sprite.update(dt) ~= nil and sprite.kill == false then
+			sprite.update(dt)
+		end
+	end
+end
+
+----Kills all sprites so best suited for screen leaving for example
+function this.kill_sprites(p_table)
+	local n
+	for n, sprite in ipairs(p_table) do
+		if sprite.kill == false then
+			sprite.kill = true
+		end
+	end
+end
+
+function this.purge_sprites(p_table)
+	local n
+	for n, sprite in ipairs(p_table) do
+		if sprite.kill == true then
+			table.remove(p_table, n)
+		end
+	end
+end
+
+function this.draw_sprites(p_table)
+	local n
+	for n, sprite in ipairs(p_table) do
+		if sprite.draw() ~= nil and sprite.kill == false then
+			sprite.draw()
+		end
+	end
+end
+
+
+return this
 
