478cd3d9b5be0ba59e5de2de3986b29dd7235073
= <slotuser@gmail.com>
2019-10-06 19:52:22 +0200

    Complete rethink of gameover screen : random text snippets, new font, random x/y text coordinates...

--- src/gameover.lua
+++ src/gameover.lua
@@ -1,11 +1,26 @@
 local def = require('define')
 
-local gameover = {}
+local this = {}
 
-function gameover.Draw()
-    love.graphics.setColor(223,0,0)
-    love.graphics.print("YOU DEAD !", def.SCREEN_WIDTH / 2 - 200, def.SCREEN_HEIGHT / 2, 0, 5, 5)
-    love.graphics.setColor(255,255,255)
+function this.load()
+	this.text = {
+		{"You may avoid\nthis kind of ground."},
+		{"You were going to die\nanyway."},
+		{"Maybe if you put\na little more effort..."},
+		{"We are all to blame."}
+	}
+
+	this.rand_n = love.math.random(1, #this.text)
+	this.rand_x = love.math.random(10, def.SCREEN_WIDTH/1.5)
+	this.rand_y = love.math.random(10, def.SCREEN_HEIGHT/1.5)
+
+   	this.font = love.graphics.newFont('assets/fonts/sixty.ttf', 340)
 end
 
-return gameover
+function this.draw()
+    love.graphics.setColor(def.color.dark_red)
+    love.graphics.print(this.text[this.rand_n], this.rand_x, this.rand_y)
+    love.graphics.setColor(1,1,1)
+end
+
+return this
